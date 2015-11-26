d5240f143e0de023cc62c0e35825d7edf9061e91
Eli Shvartsman <eli@shvar.com>
2015-11-26 18:27:05 +0200

    parsing json

--- poller.go
+++ poller.go
@@ -5,10 +5,22 @@ import(
   "net/http"
   "io/ioutil"
   "log"
+
+  "encoding/json"
 )
 
 const PollURL = "https://raw.githubusercontent.com/shvar/remedy/master/payload_example.json"
 
+type action struct {
+  Command string `json:"command"`
+  Server string `json:"server"`
+  Service string `json:"service"`
+}
+
+type operation struct {
+  Command string `json:"command"`
+  Action action `json:"payload"`
+}
 
 func main() {
   res, err := http.Get(PollURL)
@@ -18,5 +30,16 @@ func main() {
   res.Body.Close()
   if err != nil { panic(err) }
 
-  log.Printf("JSON from ruller (%x)\n", RullerLoad)
+  log.Printf("JSON from ruller (%s)\n", RullerLoad)
+  
+  WhatToDo := operation{}
+  err = json.Unmarshal(RullerLoad, &WhatToDo);
+  if err != nil { panic(err) }
+
+  log.Printf("I have to execute %s for service '%s' on server '%s' with service command '%s'\n", 
+    WhatToDo.Command, 
+    WhatToDo.Action.Service,
+    WhatToDo.Action.Server,
+    WhatToDo.Action.Command)
+
 }
