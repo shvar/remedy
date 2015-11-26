e906886b36763f98fcfab1815b21b8fb6af2e169
Eli Shvartsman <eli@shvar.com>
2015-11-26 17:38:35 +0200

    typos and imports

--- poller.go
+++ poller.go
@@ -1,15 +1,22 @@
 package main
 
-import http
+
+import( 
+  "net/http"
+  "io/ioutil"
+  "log"
+)
 
 const PollURL = "https://raw.githubusercontent.com/shvar/remedy/master/payload_example.json"
 
-res, err := http.Get(PollURL)
-if err != nil { panic(err) }
- 
-RullerLoad, err := ioutil.ReadAll(res.Body)
-res.Body.Close()
-if err != nil { panic(err) }
 
-log.Printf("JSON from ruller (%x)\n", RullerLoad)
+func main() {
+  res, err := http.Get(PollURL)
+  if err != nil { panic(err) }
+   
+  RullerLoad, err := ioutil.ReadAll(res.Body)
+  res.Body.Close()
+  if err != nil { panic(err) }
 
+  log.Printf("JSON from ruller (%x)\n", RullerLoad)
+}
